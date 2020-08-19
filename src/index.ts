392be86826d7fb293598fd4ebadb215ae363bb06
Shayne Thiessen <shayne@shaynethiessen.com>
2020-08-19 11:07:27 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {config} from 'dotenv';
 import {Client} from 'discord.js';
+import {config} from 'dotenv';
 
-config({ path: './.env' });
+config({path: './.env'});
 
 const bot = new Client();
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
@@ -9,20 +9,19 @@ const TOKEN = process.env.DISCORD_BOT_TOKEN;
 bot.login(TOKEN);
 
 bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user?.tag}!`);
+	// console.info(`Logged in as ${bot.user?.tag}!`);
 });
 
 bot.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-        // msg.channel.send('pong');
-
-    } else if (msg.content.startsWith('!kick')) {
-        if (msg.mentions.users.size) {
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`You wanted to kick: ${taggedUser?.username}`);
-        } else {
-            msg.reply('Please tag a valid user!');
-        }
-    }
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+		// msg.channel.send('pong');
+	} else if (msg.content.startsWith('!kick')) {
+		if (msg.mentions.users.size) {
+			const taggedUser = msg.mentions.users.first();
+			msg.channel.send(`You wanted to kick: ${taggedUser?.username}`);
+		} else {
+			msg.reply('Please tag a valid user!');
+		}
+	}
 });
