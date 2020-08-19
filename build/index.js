632b29fac0d3af8a31d7fe2f63cd5587604ee2fb
Shayne Thiessen <shayne@shaynethiessen.com>
2020-08-19 11:48:49 -0500

    Added hot reloading

--- build/index.js
+++ build/index.js
@@ -1,20 +1,18 @@
 "use strict";
 exports.__esModule = true;
-var dotenv_1 = require("dotenv");
 var discord_js_1 = require("discord.js");
+var dotenv_1 = require("dotenv");
 dotenv_1.config({ path: './.env' });
 var bot = new discord_js_1.Client();
 var TOKEN = process.env.DISCORD_BOT_TOKEN;
-console.log(TOKEN);
 bot.login(TOKEN);
 bot.on('ready', function () {
-    var _a;
-    console.info("Logged in as " + ((_a = bot.user) === null || _a === void 0 ? void 0 : _a.tag) + "!");
+    // console.info(`Logged in as ${bot.user?.tag}!`);
 });
 bot.on('message', function (msg) {
     if (msg.content === 'ping') {
         msg.reply('pong');
-        msg.channel.send('pong');
+        // msg.channel.send('pong');
     }
     else if (msg.content.startsWith('!kick')) {
         if (msg.mentions.users.size) {
