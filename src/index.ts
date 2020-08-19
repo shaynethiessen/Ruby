efb84bd6bdd12a20aa55939cae9b1acc580b5ef2
Shayne Thiessen <shayne@shaynethiessen.com>
2020-08-19 11:04:29 -0500

    Eslinte and Prettier

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ bot.on('ready', () => {
 bot.on('message', msg => {
     if (msg.content === 'ping') {
         msg.reply('pong');
-        msg.channel.send('pong');
+        // msg.channel.send('pong');
 
     } else if (msg.content.startsWith('!kick')) {
         if (msg.mentions.users.size) {
